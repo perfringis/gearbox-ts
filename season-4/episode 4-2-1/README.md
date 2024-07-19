f641e94dc08998d590b1d66824ba83fadbdaccfc
perfringis <kopko.mateusz@gmail.com>
2024-07-19 20:52:48 +0200

    add implementation for episode 4-2-1

---
+++ season-4/episode 4-2-1/README.md
@@ -0,0 +1,3 @@
+# Meta-model
+
+[Managing complexity through the tripartite division of logic – Open/Closed Principle in practice](https://bottega.com.pl/pdf/materialy/receptury/ocp.pdf)
---
+++ season-4/episode 4-2-1/src/Characteristics.ts
@@ -0,0 +1,37 @@
+import { RPM } from "./RPM";
+import { RpmRange } from "./RpmRange";
+
+export class Characteristics {
+  private characteristics: Object[] = [
+    2000, 1000, 1000, 0.5, 2500, 4500, 1500, 0.5, 5000, 0.7, 5000, 5000, 1500,
+    2000, 3000, 6500,
+  ];
+
+  optimalRange() {
+    return new RpmRange(
+      RPM.k(<number>this.characteristics[1]),
+      RPM.k(<number>this.characteristics[4])
+    );
+  }
+
+  optimalEcoRpmRange(): RpmRange {
+    return new RpmRange(
+      RPM.k(<number>this.characteristics[2]),
+      RPM.k(<number>this.characteristics[4])
+    );
+  }
+
+  optimalComfortRpmRange(): RpmRange {
+    return new RpmRange(
+      RPM.k(<number>this.characteristics[1]),
+      RPM.k(<number>this.characteristics[4])
+    );
+  }
+
+  optimalSportRpmRange(): RpmRange {
+    return new RpmRange(
+      RPM.k(<number>this.characteristics[14]),
+      RPM.k(<number>this.characteristics[15])
+    );
+  }
+}
